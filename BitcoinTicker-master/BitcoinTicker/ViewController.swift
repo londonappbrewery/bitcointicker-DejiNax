a385e21a496940445656c4d8408b0eabd58eafe0
DejiNax <deji.balogun10@gmail.com>
2017-10-07 13:24:23 +0100

    Add files via upload
    Assignment on Bit Coin Tracker.

---
+++ BitcoinTicker-master/BitcoinTicker/ViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  BitcoinTicker
+//
+//  Created by Angela Yu on 23/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+//  UI pickerView Delegates
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(currencyArray[row])
+        finalURL = baseURL + currencyArray[row]
+        print(finalURL)
+        symbolSelected = currencySymbolArray[row]
+        getBitCoinData (url: finalURL)
+    }
+    
+//    Variable definitions
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+//  currency selection variable
+    let currencySymbolArray = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
+    var symbolSelected = ""
+    var finalURL = ""
+
+    //Pre-setup IBOutlets
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+    }
+
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    func getBitCoinData(url: String) {
+        
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+                    
+                    print("Sucess! Got the bitcoin data")
+                    let bitCoinJSON : JSON = JSON(response.result.value!)
+                    print(bitCoinJSON)
+                    self.updateBitCoinData(json: bitCoinJSON)
+                    
+                } else {
+                    self.bitcoinPriceLabel.text = "Connection Issues"
+                }
+        }
+        
+    }
+
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    
+    func updateBitCoinData(json : JSON) {
+    
+    bitcoinPriceLabel.text = symbolSelected + json["open"]["hour"].stringValue
+
+    }
+    
+
+
+
+
+}
+
